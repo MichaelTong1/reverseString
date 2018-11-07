ae1bd7027378b9a35fef58cd5b1e4a444708aeaf
Michael Borey Tong <mbt170230@csjaws.utdallas.edu>
2018-11-07 12:03:27 -0600

    Attempted to reverse string in Java

--- main.java
+++ main.java
@@ -13,6 +13,30 @@ public class main
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World"); 
+		
+		char[] str = new char[20];
+		char[] newstr = new char[20];
+
+
+		str = "Hello World!";
+		newstr = ""; 
+
+		int beg = 0;
+		int end = str.length()-1; 
+
+		for (int i = 0; i < end/2; i++) 
+
+		{
+			
+			newstr[i] = str[end]; 
+			newstr[end] = str[i]; 
+			end--;
+		}
+
+		System.out.println("%d", newstr); 
+		
+
+
 	}
 
 }
